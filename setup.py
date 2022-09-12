76322a5976d35ee03f026cfd60614baab97aaf3f
EmbdEzaz9 <111850513+EmbdEzaz9@users.noreply.github.com>
2022-09-12 19:16:22 +0530

    Qmc5883l sources for compass sensor

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='qmc5883l',
+      version='0.92',
+      description='Python Driver for qmc5883l 3-Axis Magnetic Sensor',
+      url='http://github.com/EmbdEzaz9/Qmc5883lCompass',
+      download_url='https://files.pythonhosted.org/packages/2c/84/69e1d4e2f57cccb348ea6c1a3ff3b6b716669cc1ca591b795417cb14d75a/qmc5883l-0.92.tar.gz)',
+      author='EmbdEzaz9',
+      author_email='embdezaz@gmail.com',
+      license='MIT',
+      packages=['qmc5883l'],
+      zip_safe=False)
